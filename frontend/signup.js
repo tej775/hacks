ffca0c4aa4e3a9c1e9c1a07243142c906c23bbaa
tej775 <140933936+tej775@users.noreply.github.com>
2025-07-27 23:07:03 +0530

    Update signup.js

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,5 +1,5 @@
 // Backend API URL
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://b2b-backend-u7k0.onrender.com/api';
 
 // DOM Content Loaded
 document.addEventListener("DOMContentLoaded", function () {
